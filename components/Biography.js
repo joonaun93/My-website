99c92fc75c688c655691e918e09216751a84a5c4
Edmund Koh <edmund@Edmunds-MacBook-Air.local>
2022-07-04 11:48:35 +0800

    changed approstrophy in React html and added esLint ignore

--- components/Biography.js
+++ components/Biography.js
@@ -8,19 +8,20 @@ export default function Biography() {
         <h1>Profile</h1>
 
         <p>
-          Hello there and welcome to my site! 😊 I'm a self taught frontend web
-          developer and my tech stack is HTML, CSS, SCSS, JS, React.Js and
+          Hello there and welcome to my site! 😊 I&apos;m a self taught frontend
+          web developer and my tech stack is HTML, CSS, SCSS, JS, React.Js and
           Next.Js
         </p>
         <p>
-          I've always been fascinated by code ever since my first encounter
+          I&apos;ve always been fascinated by code ever since my first encounter
           working with the Arduino microcontroller and its native IDE during my
           undergraduate days.
         </p>
         <p>
-          After graduating in 2016, I've worked a couple of industries within
-          the Chemical Engineering field- manufacturing, wastewater, consultancy
-          - before deciding go into web development full time in November 2021.
+          After graduating in 2016, I&apos;ve worked a couple of industries
+          within the Chemical Engineering field- manufacturing, wastewater,
+          consultancy - before deciding go into web development full time in
+          November 2021.
         </p>
       </div>
       <div className="biography_image">
