b84d88cd9ff5f32d0c178c397680b1d78b02bd68
Edmund Koh <edmund@Edmunds-MacBook-Air.local>
2022-07-05 12:19:23 +0800

    added media query for mobile

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,6 +10,10 @@ export default function Navigation() {
           </a>
         </Link>
       </div>
+      <input class="side-menu" type="checkbox" id="side-menu" />
+      <label class="hamb" for="side-menu">
+        <span class="hamb-line"></span>
+      </label>
       <div className="links">
         <Link href="/contact">contact</Link>
         <Link href="/projects">projects</Link>
